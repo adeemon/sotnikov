057ec2a2b97a3ab771576325b7621133856d1b19
Ilia Ustimov <ilia_ustimov@epam.com>
2023-07-03 20:15:40 +0400

    Буду переделывать логику

--- src/components/layout/post/Post.jsx
+++ src/components/layout/post/Post.jsx
@@ -1,29 +1,29 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
+import { useInView } from 'react-intersection-observer';
 
-import { selectUserById, selectUsers } from '../../../redux/slices/UsersSlice';
-import styles from './styles.module.scss'
-import classNames from 'classnames';
+import { selectUserById } from '../../../redux/slices/UsersSlice';
 
-import { useForm } from "react-hook-form"
+
 import { deletePost, updatePost } from '../../../redux/slices/PostsSlice';
-import { Comments } from '../comments/Comments';
+
 import { addFavourite, removeFavourite, selectIsFavourite } from '../../../redux/slices/FavouritesSlice';
-import { FavouriteButton } from '../../ui-kit/buttons/FavouriteButton';
-import { CommentsButton } from '../../ui-kit/buttons/CommentsButton';
-import { DeleteButton } from '../../ui-kit/buttons/DeleteButton';
-import { EditButton } from '../../ui-kit/buttons/EditButton';
+import { PostView } from './PostView';
 
 export function Post({userId, id, title, body}) {
 
-  const { register, handleSubmit } = useForm();
   let [editMode, setEditMode] = useState(false);
   let [isCommentsOpened, setIsCommentsOpened] = useState(false);
   
   const dispatch = useDispatch();
   const userNameSelector = useSelector(selectUserById(userId));
   const isFavourite = useSelector(selectIsFavourite(id));
+
+  const { ref, inView, entry } = useInView({
+    /* Optional options */
+    threshold: 0,
+  });
 
   const onComments = () => {
     setIsCommentsOpened(!isCommentsOpened);
@@ -47,47 +47,30 @@ export function Post({userId, id, title, body}) {
     dispatch(deletePost(id));
   }
 
-  const onSubmit = (data) => {
-    dispatch(updatePost({userId, id, ...data}))
+  const onSubmit = () => {
     setEditMode(false);
   }
 
   return (
-    <div className={styles.container}> 
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className={ styles.info }>
-          <input 
-          tabIndex={editMode ? 0 : -1} 
-          className={classNames(styles.title, editMode ? styles.editable : null)} {...register('userName')} 
-          defaultValue={userNameSelector} 
-          readOnly={!editMode} />
-          <input
-            tabIndex={editMode ? 0 : -1}
-            className={classNames(styles.email, editMode ? styles.editable : null)} {...register('title')} 
-            defaultValue={title} 
-            readOnly={!editMode} />
-          <input
-            tabIndex={editMode ? 0 : -1}
-            className={classNames(styles.body, editMode ? styles.editable : null)} {...register('body')} 
-            defaultValue={body} 
-            readOnly={!editMode} />
-        </div>
-      <input className={!editMode ? styles.hidden : null}
-        type="submit"
-        value="Send"
+    <div ref={ref}>
+      {inView
+      ? <PostView 
+      editMode = {editMode}
+      isCommentsOpened = {isCommentsOpened}
+      isFavourite = {isFavourite}
+      userName = {userNameSelector}
+      title = {title}
+      body = {body}
+      onComments = {onComments}
+      onEdit = {onEdit}
+      onFavourite = {onFavourite}
+      onDelete = {onDelete}
+      onSubmit = {onSubmit}
+      id = {id}
+      key = {id}
+      userId = {userId}
       />
-      <input className={!editMode ? styles.hidden : null}
-        type="reset"
-        value="Reset"
-      />
-    </form>
-    <CommentsButton onClick={onComments} isActive={isCommentsOpened}/>
-    <EditButton onClick={onEdit} />
-    <FavouriteButton isFavourite={isFavourite} onClick={onFavourite} />
-    <DeleteButton onClick={onDelete} />
-    {isCommentsOpened
-    ? <Comments postId={id}/>
-    : null}
+      : null}
     </div>
   );
 }
